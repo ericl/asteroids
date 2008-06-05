2115e13836ba79507c17f21666b6d23baf3b906a
Will <redfedora404@gmail.com>
2008-06-04 21:17:49 -0700

    new asteroids; non_functional but existant missile

--- asteroids/weapons/Missile.java
+++ asteroids/weapons/Missile.java
@@ -1,4 +1,6 @@
 package asteroids.weapons;
+import java.util.List;
+
 import asteroids.display.*;
 import static asteroids.Util.*;
 import net.phys2d.raw.*;
@@ -6,19 +8,54 @@ import net.phys2d.math.*;
 import net.phys2d.raw.shapes.*;
 
 
-public class Missile extends Body implements Textured {
-	private static ROVector2f[] mShape = { v(0,0), v(4,0), v(0,-6), v(-8,0), v(0,6), v(4,4) };
+public class Missile extends Weapon {
+	protected Body myTarget;
 	
 	public Missile() {
-		super(new Polygon(mShape), 10);
+		super(new Circle(10));
+	}
+	
+	public Body getRemnant() {
+		return new LaserExplosion();
+	}
+	
+	public List<Body> getFragments() {
+		return null;
+	}
+	
+	public void setTarget(Body b) {
+		myTarget = b;
+	}
+	
+	public Body getTarget() {
+		return myTarget;
+	}
+	
+	public int getBurstLength() {
+		return 0;
+	}
+	public float getDamage() {
+		return .1f;
+	}
+	
+	public void endFrame() {
+		
+	}
+	
+	public float getReloadTime() {
+		return 300;
+	}
+	
+	public float getSpeed() {
+		return 75;
 	}
 	
 	public String getTexturePath() {
-		return "pixmaps/circle1.png";
+		return "pixmaps/laser.png";
 	}
 
 	public float getTextureScaleFactor() {
-		return 10 / 41.0f;
+		return 2;
 	}
 
 	public Vector2f getTextureCenter() {
